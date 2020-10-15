c2959f597203fcbb8c5ce778c924587f7c69d1eb
llenn4ik <llenn4ik@gmail.com>
2020-10-15 18:30:53 +0200

    Updates

---
+++ precache-manifest.a42e1cf3bc1d620b6a4a8caffb850968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe7735ffe7f098b05535ccbb9101c76",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "3c8c4baa38d9ccc7f459",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.363aa1e5.chunk.css"
+  },
+  {
+    "revision": "8dac924391677f252973",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d145d93d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d145d93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8c4baa38d9ccc7f459",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.3a06eb2d.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
